a13173ba39dec04201a45f02e331630d3bbfdab8
Hyekang Joo <joos2010kj@gmail.com>
2022-03-03 01:04:43 -0500

    Update API key

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 ROOT_URL=https://api.radiant.earth/mlhub/v1/archive
-KEY=17238db9dae8c654b106ec7ab1c10ccb8613176aaeca8fd30ad2806b5e93b87c
+KEY=16a2d3dd90787e39dbc13979a7d7187f71528bf66f03ef7eff9f6af5c5d18065
 
 curl -L "$ROOT_URL/$1?key=$KEY" --output data/$1.tar.gz
