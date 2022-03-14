8d1a0e8fe45024db2905e9193b53c7e9ff5c8bcf
Ivan <ivan.zvonkov@gmail.com>
2022-03-14 01:49:39 -0400

    Some updates to sbatch files

---
+++ sb4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --output=slurm/%j.out                              
+#SBATCH --error=slurm/%j.out                                 
+#SBATCH --time=48:00:00
+#SBATCH --account=scavenger
+#SBATCH --partition scavenger
+#SBATCH --gres=gpu:4
+#SBATCH --mem=32G
+#SBATCH --exclude=cmlgrad02,cml12,cmlgrad05
+
+source venv/bin/activate
+srun "$@"
